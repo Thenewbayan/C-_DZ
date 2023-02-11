5721a697bb628cccde2b00bf14c88c468b5db43d
Thenewbayan <chemspirit.trade@gmail.com>
2023-02-11 23:13:53 +0300

    vse gotovo

---
+++ numbers_2.cs
@@ -0,0 +1,20 @@
+﻿//Прога для поиска максимального из вводимых чисел
+
+Console.WriteLine("Введите число");
+int numberA=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число и программа выдаст которое из них больше и какое меньше");
+int numberB=Convert.ToInt32(Console.ReadLine());
+//Ввод исходных данных, дальше код самой проги
+if (numberA>numberB)
+{
+    Console.WriteLine("Число "+numberA+" является большим из заданных, а число "+numberB+" меньшим");
+};
+//первое условие
+if (numberB>numberA)
+{
+    Console.WriteLine("Число "+numberB+" является большим из заданных, а число "+numberA+" меньшим");
+};//второе условие
+if(numberA==numberB)
+{
+    Console.WriteLine("Хитро! Вы задали одинаковые числа");
+};// vse rabotaet!
