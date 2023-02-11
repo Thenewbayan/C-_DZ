1775840493c45d0018adc013a1b5118cc18ef9ad
Thenewbayan <chemspirit.trade@gmail.com>
2023-02-11 23:39:30 +0300

    chet/nechet

---
+++ chet_nechet/chet_nechet.cs
@@ -0,0 +1,14 @@
+﻿// проверка четности
+
+Console.WriteLine("Введите число, а программа скажет четное ли оно");
+int number=Convert.ToInt32(Console.ReadLine());
+//vvod dannih zakonchen
+
+if(number%2==0)
+{
+    Console.WriteLine("число "+number+" четное");
+}
+else
+{
+    Console.WriteLine("число "+number+" нечетное");
+};
