aaf28cf75cea10fcdf0808f229d4d9bbc8b7d8da
Thenewbayan <chemspirit.trade@gmail.com>
2023-02-11 23:51:21 +0300

    poisk chetnuh chisel OK

---
+++ chetnye_do_chisla/chetnye.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("ДАвайте найдем все четные числа от 2 до вашего числа, введите число");
+int number=Convert.ToInt32(Console.ReadLine());
+int count=2;
+while(count<=number)
+{
+    Console.WriteLine(count);
+    count=count+2;
+};
